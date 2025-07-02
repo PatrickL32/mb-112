ae13a1256846aa07f59beda8514496a130e01eeb
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-07-01 19:03:38 -0700

    2nd commit

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from pages.views import HomePageView, AboutPageView
 
-urlpatterns = [
+urlpatterns = (
     path("", HomePageView.as_view(), name="home"),
-    path("about/" , AboutPageView.as_view(),name="about")
-]
+    path("about/" , AboutPageView.as_view(),name="about"),
+)
